6b4a7e17403eb8c95827a46eee884f7bb0763994
Gagandeep Singh <deepgagan9336@gmail.com>
2021-02-22 22:50:25 +0530

    first commit

--- account.js
+++ account.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendgridAPIkey = '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';
+const sendgridAPIkey = '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';
 
 sgMail.setApiKey(sendgridAPIkey);
 
@@ -10,7 +10,12 @@ const sendWelcomeEmail = (name, email, message) => {
         from: email,
         subject: `Hey my name is ${name}`,
         text: message
-    });
+    }).then(() => {
+        console.log('Email sent')
+    })
+        .catch((error) => {
+            console.error(error)
+        });
 }
 
 module.exports = {
